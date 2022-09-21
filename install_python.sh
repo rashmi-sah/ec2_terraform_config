85372c8f0439ad817222dfd747027aee5adf4b20
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-21 17:16:36 +0530

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -1,7 +1,7 @@
-	#echo "############ Installing pip ###########################"
-	#echo "################ /n/n/n/n/n/n/n/n/n/n/################"
-	#sudo apt-get -y install python3
+	echo "############ Installing pip ###########################"
+	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
+	sudo apt install -y python3
 	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
 	echo "############ Installing pip ###########################"
-	sudo apt-get -y install python3-pip
+	sudo apt install -y python3-pip
 	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
