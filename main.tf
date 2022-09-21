054158378bc014b46059e65de84241be84d4575e
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-21 13:00:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "null_resource" "do_nothing" {
     type="ssh"
     user = "ubuntu"
     host="54.91.187.112"
-    private_key=file("/home/runner/work/ec2_terraform_config_remote_exec/ec2_terraform_config_remote_exec/private_key.ppk")
+    private_key=file("/home/runner/work/ec2_terraform_config_remote_exec/ec2_terraform_config_remote_exec/prv_key.ppk")
     #agent = true
     timeout = "4m"
   }
