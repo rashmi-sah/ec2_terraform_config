7b4d8256fbf6efd7d97ff4f89c594400b96575ac
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-21 15:39:47 +0530

    Update and rename install_docker.sh to install_python.sh

---
+++ install_python.sh
@@ -0,0 +1,7 @@
+	echo "############ Installing pip ###########################"
+	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
+	sudo apt-get -y install python3
+	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
+	echo "############ Installing pip ###########################"
+	sudo apt-get -y install python3-pip
+	echo "################ /n/n/n/n/n/n/n/n/n/n/################"
